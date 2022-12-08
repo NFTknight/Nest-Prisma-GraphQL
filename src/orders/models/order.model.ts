00b4ca0107ce8624ccbeadf31a39cb8fd6f277f3
NFT Knight <howardjohn855@gmail.com>
2022-12-08 21:24:13 +0500

    order model

--- src/orders/models/order.model.ts
+++ src/orders/models/order.model.ts
@@ -1,21 +1,32 @@
-import { Field, ObjectType, Float } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { DeliveryMethods, OrderStatus, PaymentMethods } from '@prisma/client';
+import { IsString } from 'class-validator';
 import { Cart } from 'src/cart/models/cart.model';
 import { BaseModel } from 'src/common/models/base.model';
 import { Vendor } from 'src/vendors/models/vendor.model';
+import { CustomerInfo } from './customer-info.model';
 
 @ObjectType()
 export class Order extends BaseModel {
-  @Field(() => Float)
-  totalAmount: number;
+  @Field(() => CustomerInfo)
+  customerInfo: CustomerInfo;
 
-  @Field(() => Float)
-  finalAmount: number;
-
+  @IsString()
   cartId: string;
   @Field(() => Cart, { nullable: false })
-  Cart?: Cart;
+  cart?: Cart;
 
+  @IsString()
   vendorId: string;
   @Field(() => Vendor, { nullable: false })
-  Vendor?: Vendor;
+  vendor?: Vendor;
+
+  @Field(() => OrderStatus, { nullable: false })
+  status: OrderStatus;
+
+  @Field(() => DeliveryMethods)
+  deliveryInfo?: DeliveryMethods;
+
+  @Field(() => PaymentMethods)
+  paymentInfo?: PaymentMethods;
 }
