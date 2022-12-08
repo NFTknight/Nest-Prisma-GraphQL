2ea8a74c2349649f2de31c24fca1eda3dcca6bf7
NFT Knight <howardjohn855@gmail.com>
2022-12-08 21:25:03 +0500

    order resolvers and services

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CartModule } from 'src/cart/cart.module';
-import { CartService } from 'src/cart/services/cart.service';
+import { VendorsModule } from 'src/vendors/vendors.module';
+import { OrdersResolver } from './order.resolver';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [CartModule],
-  providers: [OrdersService],
+  imports: [VendorsModule, CartModule],
+  providers: [OrdersService, OrdersResolver],
   exports: [OrdersService],
 })
 export class OrdersModule {}
