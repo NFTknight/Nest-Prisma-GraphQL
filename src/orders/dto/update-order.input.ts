26494e068033397ee118d6dd5bf0a374fc2264ea
NFT Knight <howardjohn855@gmail.com>
2022-12-09 15:55:11 +0500

    removed comments

--- src/orders/dto/update-order.input.ts
+++ src/orders/dto/update-order.input.ts
@@ -1,28 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
 import { DeliveryMethods, PaymentMethods, OrderStatus } from '@prisma/client';
-import { IsEmail } from 'class-validator';
-
-@InputType()
-class UpdateCustomerInput {
-  @Field()
-  firstName?: string;
-
-  @Field()
-  lastName?: string;
-
-  @Field()
-  address?: string;
-
-  @Field()
-  phone?: string;
-
-  @Field()
-  @IsEmail()
-  email?: string;
-
-  @Field()
-  city?: string;
-}
 
 @InputType()
 export class UpdateOrderInput {
@@ -34,9 +11,6 @@ export class UpdateOrderInput {
 
   @Field()
   cartId?: string;
-
-  // @Field()
-  // customerInfo?: UpdateCustomerInput;
 
   @Field(() => PaymentMethods)
   paymentMethod?: PaymentMethods;
