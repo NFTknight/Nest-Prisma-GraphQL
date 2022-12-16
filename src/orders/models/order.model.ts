f3cdfde24318f227fd7e4e6965f28cc78e95bde7
NFT Knight <howardjohn855@gmail.com>
2022-12-16 19:24:49 +0500

    update order modal

--- src/orders/models/order.model.ts
+++ src/orders/models/order.model.ts
@@ -3,6 +3,7 @@ import { DeliveryMethods, OrderStatus, PaymentMethods } from '@prisma/client';
 import { IsString } from 'class-validator';
 import { Cart } from 'src/cart/models/cart.model';
 import { BaseModel } from 'src/common/models/base.model';
+import { WayBill } from 'src/shipping/models/waybill.model';
 import { Vendor } from 'src/vendors/models/vendor.model';
 import { CustomerInfo } from './customer-info.model';
 
@@ -41,4 +42,6 @@ export class Order extends BaseModel {
 
   @Field(() => [FormResponse])
   formResponses?: FormResponse[];
+  @Field(() => WayBill, { nullable: true })
+  waybill?: WayBill;
 }
