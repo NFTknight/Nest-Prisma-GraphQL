ed0c21a31a89b9d2f11b0a89d3da6f8b10776474
NFT Knight <howardjohn855@gmail.com>
2022-12-29 19:56:13 +0500

    delete paginatedBooking type.

--- src/bookings/models/paginated-bookings.model.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-import paginated from 'src/common/pagination/pagination';
-import { Booking } from './booking.model';
-
-@ObjectType()
-export class PaginatedBookings extends paginated(Booking) {}
