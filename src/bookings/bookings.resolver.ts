177f84d00f19453163f5b67a9596503cb4a96ad3
NFT Knight <howardjohn855@gmail.com>
2023-01-06 06:26:19 +0100

    fixed null value for orders and tags with getBooking Query

--- src/bookings/bookings.resolver.ts
+++ src/bookings/bookings.resolver.ts
@@ -19,7 +19,10 @@ import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
 import { PaginationArgs } from 'src/common/pagination/pagination.input';
 import { PaginatedBookings } from './models/paginated-bookings.model';
 import { SortOrder } from 'src/common/sort-order/sort-order.input';
-import { Vendor } from '@prisma/client';
+import { Vendor, Order } from '@prisma/client';
+import { OrdersService } from 'src/orders/orders.service';
+import { TagsService } from 'src/tags/tags.service';
+import { Tag } from 'src/tags/models/tag.model';
 
 @Resolver(() => Booking)
 export class BookingResolver {
@@ -27,7 +30,9 @@ export class BookingResolver {
     private readonly cartService: CartService,
     private readonly vendorService: VendorsService,
     private readonly productService: ProductsService,
-    private readonly bookingService: BookingsService
+    private readonly bookingService: BookingsService,
+    private readonly ordersService: OrdersService,
+    private readonly tagService: TagsService
   ) {}
 
   @Query(() => Booking)
@@ -99,5 +104,15 @@ export class BookingResolver {
   @ResolveField('vendor')
   vendor(@Parent() booking: Booking): Promise<Vendor> {
     return this.vendorService.getVendor(booking.vendorId);
+  }
+
+  @ResolveField('order')
+  order(@Parent() booking: Booking): Promise<Order> {
+    return this.ordersService.getOrder(booking.orderId);
+  }
+
+  @ResolveField('tag')
+  tag(@Parent() booking: Booking): Promise<Tag> {
+    return this.tagService.getTag(booking.tagId);
   }
 }
