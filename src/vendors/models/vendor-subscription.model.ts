93b66e06627fe10bbf714db58aef35814b973331
NFT Knight <howardjohn855@gmail.com>
2023-01-06 17:19:54 +0500

    addOnList type added.

--- src/vendors/models/vendor-subscription.model.ts
+++ src/vendors/models/vendor-subscription.model.ts
@@ -3,6 +3,22 @@ import { SubscriptionPlan, SubscriptionType } from '@prisma/client';
 import './vendor.enum';
 
 @ObjectType()
+export class AddOn {
+  feature: string;
+  @Field(() => Float)
+  price: number;
+}
+@ObjectType()
+export class AddOnsList {
+  @Field(() => [AddOn])
+  list: AddOn[];
+  @Field(() => Float)
+  totalMonthly: number;
+  @Field(() => Float)
+  totalYearly: number;
+}
+
+@ObjectType()
 export class VendorSubscription {
   @Field(() => SubscriptionPlan)
   plan: SubscriptionPlan;
@@ -18,4 +34,7 @@ export class VendorSubscription {
 
   @Field()
   createdAt: Date;
+
+  @Field(() => AddOnsList, { nullable: true })
+  addOns: AddOnsList;
 }
