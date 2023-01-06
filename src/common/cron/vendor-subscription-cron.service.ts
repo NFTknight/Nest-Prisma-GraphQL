f188394b8e865ef7501351efb01763f8f717bdea
NFT Knight <howardjohn855@gmail.com>
2023-01-06 16:19:49 +0500

    cron job added to decrement free time left.

--- src/common/cron/vendor-subscription-cron.service.ts
+++ src/common/cron/vendor-subscription-cron.service.ts
@@ -1,12 +1,33 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class VendorSubscriptionService {
   private readonly logger = new Logger(VendorSubscriptionService.name);
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  handleCron() {
-    this.logger.debug('Called every 30 seconds');
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async handleCron() {
+    await this.prisma.vendor.updateMany({
+      where: {
+        subscription: {
+          is: {
+            freeTimeLeft: { gt: 0 },
+          },
+        },
+      },
+      data: {
+        subscription: {
+          upsert: {
+            set: null,
+            update: {
+              freeTimeLeft: { decrement: 1 },
+            },
+          },
+        },
+      },
+    });
+    this.logger.debug('Called every day at midnight');
   }
 }
