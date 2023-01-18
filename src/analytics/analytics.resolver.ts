a1f6d633be15cb13559a01178060ed45dd9c7c4c
NFT Knight <howardjohn855@gmail.com>
2023-01-18 13:44:46 +0500

    added guard in analytics

--- src/analytics/analytics.resolver.ts
+++ src/analytics/analytics.resolver.ts
@@ -1,5 +1,8 @@
+import { SetMetadata, UseGuards } from '@nestjs/common';
 import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Role } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
+import { RolesGuard } from 'src/auth/gql-signup.guard';
 import { AnalyticsService } from './analytics.service';
 import { Analytics } from './models/analytics.models';
 
@@ -10,11 +13,15 @@ export class AnalyticsResolver {
     private readonly prisma: PrismaService
   ) {}
 
+  @UseGuards(RolesGuard)
+  @SetMetadata('role', Role.ADMIN)
   @Query(() => Analytics)
   getAnalytics(@Args('vendorId') id: string): Promise<Analytics> {
     return this.analyticsService.getAnalytics(id);
   }
 
+  @UseGuards(RolesGuard)
+  @SetMetadata('role', Role.ADMIN)
   @Query(() => Int)
   numberOfDroppedBaskets(@Args('vendorId') id: string): Promise<number> {
     return this.analyticsService.numberOfDroppedBaskets(id);
