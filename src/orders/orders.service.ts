59c7af01486fa0629b69c510f7f7e7c6f1e59f2d
NFT Knight <howardjohn855@gmail.com>
2023-02-15 23:40:50 +0300

    change url of somatcha email

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -257,7 +257,7 @@ export class OrdersService {
                 '',
                 {
                   orderID: order.id,
-                  url: `https://anyaa.io/somatcha/checkout/${order.id}/confirmation?lng=en`,
+                  url: `https://anyaa.io/qrcode/${order.id}?lng=en`,
                   workshop: {
                     title: product.title,
                     title_ar: product.title_ar,
